6d6dfe405af28f8074c4cb2e3b2f371c8c2c74af
Noteb <pabloandressilva99@gmial.com>
2023-09-11 20:34:47 -0300

    entities

--- server/src/domain/entities/category.entities.ts
+++ server/src/domain/entities/category.entities.ts
@@ -2,5 +2,19 @@ class Category {
   private id: string;
   private name: string;
   private color: string;
+
+  private constructor(
+   id: string,
+   name: string,
+   color: string
+  )
+  {
+    this.id = id;
+    this.name = name;
+    this.color = color;    
+  }
+  public getId(): string {
+    return this.id;
+  }
 }
 export default Category;
--- server/src/domain/entities/claim.entities.ts
+++ server/src/domain/entities/claim.entities.ts
@@ -14,5 +14,28 @@ class Claim {
   private location: string;
   private createdAT: Date;
   private cloneOf?: Claim;
+
+  private constructor(
+    id: string,
+    owner: Visitor,
+    title: string,
+    description: string,
+    category: Category,
+    location: string,
+    createdAT: Date,
+    cloneOf?: Claim
+  ){
+    this.id = id;
+    this.owner = owner;
+    this.title = title;
+    this.description = description;
+    this.category = category;
+    this.location = location;
+    this.createdAT = createdAT;
+    this.cloneOf = cloneOf;
+  }
+  public getId(): string {
+    return this.id;
+  }
 }
 export default Claim;
