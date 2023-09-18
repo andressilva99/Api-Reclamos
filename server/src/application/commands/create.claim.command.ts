e338be69e4392865bb5dce4b010fe131125abcaa
Francolesca <88465161+Francolesca@users.noreply.github.com>
2023-09-18 20:36:45 -0300

    ClaimCommand

---
+++ server/src/application/commands/create.claim.command.ts
@@ -0,0 +1,45 @@
+export class CreateClaimCommand {
+
+    private readonly ownerId: string;
+    private readonly title: string;
+    private readonly description: string;
+    private readonly categoryId: string;
+    private readonly location: string;
+    private readonly createdAT: Date;
+
+    constructor( 
+        ownerId: string,
+        title: string,
+        description: string,
+        categoryId: string,
+        location: string,
+        createdAT: Date,
+        ) {
+            this.ownerId = ownerId;
+            this.title = title;
+            this.description = description;
+            this.categoryId = categoryId;
+            this.location = location;
+            this.createdAT = createdAT;
+    }
+
+    getOwner():string{
+        return this.ownerId;
+    }
+    getTitle():string{
+        return this.title;
+    }
+    getDescription():string{
+        return this.description;
+    }
+    getCategory():string{
+        return this.categoryId;
+    }
+    getLocation():string{
+        return this.location;
+    }
+    getCreatedAT():Date{
+        return this.createdAT;
+    }
+
+}
