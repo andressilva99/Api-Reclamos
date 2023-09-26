b6b6cdf7cc1433a1aea3b14b1b4abbbb46e49c73
juanmarcosls <juanmarcoslemossmit@gmail.com>
2023-09-26 11:02:30 -0300

    Creando categories-seeder

---
+++ server/src/infrastructure/seeders/categories-seeder.ts
@@ -0,0 +1,22 @@
+import { Categories } from '../../domain/entities/category.entities';
+import categoriesRepository from '../repositories/category.repository';
+
+class CategoriesSeeder {
+  private categories: Array<Categories> = [];
+  
+  public constructor() {
+    this.categories.push(Categories.create('VIP', 5025));
+    this.categories.push(Categories.create('clase media', 9050));
+    this.categories.push(Categories.create('primera clase', 9506));
+    this.categories.push(Categories.create('categoria B', 7894));
+    this.categories.push(Categories.create('categoria C', 1236));
+  }
+
+  public async generate(): Promise<void> {
+    for (const categories of this.categories) {
+      await categoriesRepository.save(categories);
+    }
+  }
+}
+
+export default new CategoriesSeeder();
