d3997cabaa8ff95586f4100dbf73636bfc1edd5d
FrancoAcevedoE <frankling1521@gmail.com>
2023-09-29 19:56:27 -0300

    create visitor handler

--- server/src/application/handlers/create.visitor.handlers.ts
+++ server/src/application/handlers/create.visitor.handlers.ts
@@ -1,3 +1,16 @@
-export default class createVisitorHandler {
+import Visitor from "../../domain/entities/visitor.entities";
+import visitorRepository from "../../infrastructure/repositories/visitor.repository";
+import { CreateVisitorCommand } from "../commands/create.visitor.command";
 
+class createVisitorHandler  {
+    async execute(command: CreateVisitorCommand) {
+        const user = Visitor.create(
+          command.getId(),
+          command.getIp(),
+          command.getNickName()
+        );
+    
+        await visitorRepository.save(user);
+      }
 }
+export default new createVisitorHandler();
