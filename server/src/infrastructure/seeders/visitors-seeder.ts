1a238dc736a9ab1cacffbebe6ce31a879634ab49
Noteb <pabloandressilva99@gmial.com>
2023-10-03 15:31:43 -0300

    creacion de visitors seeder

---
+++ server/src/infrastructure/seeders/visitors-seeder.ts
@@ -0,0 +1,20 @@
+import Visitor from "domain/entities/visitor.entities";
+import visitorRepository from "infrastructure/repositories/visitor.repository";
+
+class VisitorSeeder {
+    private visitor: Array<Visitor> = [];
+    public constructor() {
+        this.visitor.push(Visitor.create('interno', 3782))
+        this.visitor.push(Visitor.create('receptor', 3412))
+        this.visitor.push(Visitor.create('emisor', 2714))
+    }
+
+    public async generate(): Promise<void>{
+        for (const visitor of this.visitor){
+            await visitorRepository.save(visitor);
+        }
+    }
+}
+
+export default new VisitorSeeder();
+ 
