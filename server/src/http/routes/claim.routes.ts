776ce7d36f93d204ef44987c1fd254073440f43d
Francolesca <88465161+Francolesca@users.noreply.github.com>
2023-10-24 20:27:23 -0300

    GetClaimAction-rutaConfigurada

--- server/src/http/routes/claim.routes.ts
+++ server/src/http/routes/claim.routes.ts
@@ -1,6 +1,7 @@
 import { Application } from "express";
 import CommonRoutes from "./common.routes";
 import createClaimAction from "../actions/create.claim.action";
+import getLastClaimsAction from "../actions/get.last.claims.action"
 
 
 class ClaimRoutes extends CommonRoutes {
@@ -10,6 +11,7 @@ class ClaimRoutes extends CommonRoutes {
 
     setUpRoutes():Application {
         this.app.post('/claim', createClaimAction.run);
+        this.app.get('/claim', getLastClaimsAction.run);
         return this.app;
     }
 
