60d7eedc95b5567059b0372d385b028546a52b9d
Francolesca <88465161+Francolesca@users.noreply.github.com>
2023-10-30 20:31:14 -0300

    Creacion ReportClaim Archivos

---
+++ server/src/http/actions/report.claim.action.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import { ReportClaimCommand } from "../../application/commands/report.claim.command";
+import { ReportClaimHandler } from "../../application/handlers/report.claim.handlers";
+
+class ReportClaimAction {
+    async run (req: Request, res: Response) {
+        
+    }
+}
