19e643210b59a25b2a56f10b8f236596955f927a
FrancoAcevedoE <frankling1521@gmail.com>
2023-10-30 21:06:35 -0300

    Cambiamos los valores de like por dislike

--- server/src/http/actions/dislike.action.ts
+++ server/src/http/actions/dislike.action.ts
@@ -1,25 +1,26 @@
 import { Request, Response } from "express";
-import { DisLikeCommand } from "../../application/commands/dislike.command";
-import likeHandlers from "../../application/handlers/like.handlers";
+import { DislikeCommand } from "../../application/commands/dislike.command";
+import DislikeHandlers from "../../application/handlers/dislike.handlers";
 
 class DislikeAction {
-  async run(req: Request, res: Response) {
-    try {
-      const like = new DisLikeCommand(
-        req.params.idClaim,
-        req.params.idVisitor,
-        req.params.pinVisitor
-      );
+    async run(req: Request, res: Response){
+        try{
+            const dislike = new DislikeCommand(
+                req.params.idClaim,
+                req.params.idVisitor,
+                req.params.pinVisitor
+            );
 
-      try {
-        await likeHandlers.execute(like);
-      } catch (error) {
-        return res.status(404).json({ message: "ERROR" });
-      }
-    } catch (e) {
-      console.log(e);
+            try {
+                await DislikeHandlers.execute(dislike)
+            }
+            catch (error){
+                return res.status(404).json({message: "ERROR"});
+            }
+        }catch (e){
+            console.log(e)
+        }
     }
-  }
 }
 
 export default DislikeAction;
