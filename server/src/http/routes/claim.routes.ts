4f5231d58d1a78599a00d8a421ecfe1b5234e846
= <franco.sistemas1999@gmail.com>
2023-11-01 20:19:07 -0300

    error corregido

--- server/src/http/routes/claim.routes.ts
+++ server/src/http/routes/claim.routes.ts
@@ -1,8 +1,8 @@
 import { Application } from "express";
 import CommonRoutes from "./common.routes";
 import createClaimAction from "../actions/create.claim.action";
-import getLastClaimsAction from "../actions/get.last.claims.action"
-import ReportClaimAction from "../actions/report.claim.action"
+import getLastclaimsAction from "../actions/get.lastclaims.action";
+import ReportClaimAction from "../actions/report.claim.action";
 
 class ClaimRoutes extends CommonRoutes {
     constructor(app: Application) {
@@ -11,7 +11,7 @@ class ClaimRoutes extends CommonRoutes {
 
     setUpRoutes():Application {
         this.app.post('/claim', createClaimAction.run);
-        this.app.get('/claim', getLastClaimsAction.run);
+        this.app.get('/claim', getLastclaimsAction.run);
         this.app.put('/claim', ReportClaimAction.run);
         return this.app;
     }
