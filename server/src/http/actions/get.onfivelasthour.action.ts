60084b4715af8199c00be994a58ebbda7ee4500d
ThomasLlopiz <toomillopiz@gmai.com>
2023-11-02 08:46:45 -0300

    onfivelasthour

---
+++ server/src/http/actions/get.onfivelasthour.action.ts
@@ -0,0 +1,21 @@
+import claimRepository from "../../infrastructure/repositories/claim.repository ";
+import Claim from "../../domain/entities/claim.entities";
+import { Request, Response } from "express";
+
+class GetOnFireActionLastHout {
+  async run(req: Request, res: Response) {
+    try {
+      const getonfireaction: Claim[] =
+        await claimRepository.listTop5ByLikesInLastHour();
+      return res.status(200).json(getonfireaction);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        error:
+          "Ocurrio un error al obtener los últimos cinco con más me gusta de la última hora",
+      });
+    }
+  }
+}
+
+export default new GetOnFireActionLastHout();
