877f7a98183ef4fdae2deff76e404eaec2b450f9
ThomasLlopiz <toomillopiz@gmai.com>
2023-11-10 10:56:09 -0300

    add seeders and routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import CommonRoutes from "./http/routes/common.routes";
+import CategoryRoutes from "./http/routes/category.routes";
+import VisitorRoutes from "./http/routes/visitor.routes";
+import ClaimRoutes from "./http/routes/claim.routes";
 import cors from "cors";
 import { log } from "debug";
 import expressWinston from "express-winston";
 import winston from "winston";
+import visitorsSeeder from "./infrastructure/seeders/visitors.seeder";
+import claimSeeder from "./infrastructure/seeders/claims.seeder";
+import categoriesSeeder from "./infrastructure/seeders/categories.seeder";
 
 const app: express.Application = express();
 
@@ -20,7 +26,17 @@ if (!process.env.DEBUG) {
   loggerOptions.meta = false; // when not debugging, log requests as one-liners
 }
 
+try {
+  categoriesSeeder.generate();
+  visitorsSeeder.generate();
+  claimSeeder.generate();
+} catch {
+  console.log('error generating seeders');
+}
+
+
 app.use(expressWinston.logger(loggerOptions));
+
 
 const routes: Array<CommonRoutes> = [];
 app.use(cors());
@@ -30,6 +46,10 @@ app.use(express.json());
 // you should add your routes here...
 //routes.push(new PassengerRoutes(app)); crear una nueva estancia del la ruta y push
 //routes.push()
+
+routes.push(new CategoryRoutes(app));
+routes.push(new VisitorRoutes(app));
+routes.push(new ClaimRoutes(app));
 
 app.listen(3000, () => {
   routes.forEach((route: CommonRoutes) => {
